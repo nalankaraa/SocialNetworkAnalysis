f1ef4aea6bec366a6fab46773bb650062c1861bf
nalankaraa <nalankaraa3@gmail.com>
2025-12-30 15:40:34 +0300

    added edge and node property panels

---
+++ SocialNetworkAnalysis.UI/Views/NodePropertiesPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SocialNetworkAnalysis.UI.Views
+{
+    public partial class NodePropertiesPanel : UserControl
+    {
+        public NodePropertiesPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
+
